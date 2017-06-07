8120850ccaf49821647e4b6794d822f25e947008
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-06-07 06:59:22 +0300

    Wrap admin.js by self-executing anonymous function
    Otherwise we have conflicts. For instance it defines window.opera causes problems with other libraries where they check if current browser is opera.

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,5 @@
+(function() {
+
 if (typeof jQuery === "undefined") {
     throw new Error("jQuery plugins need to be before this file");
 }
@@ -461,3 +463,5 @@ $(function () {
 
     setTimeout(function () { $('.page-loader-wrapper').fadeOut(); }, 50);
 });
+
+})();
