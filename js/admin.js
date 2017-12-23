303e4e991f22a174362e7e022f0c0a31a28f7556
geoffrey-brier <geoffrey.brier@gmail.com>
2017-12-23 08:53:25 +0100

    Fix JS error when there is no active menu
    Everything is in the title, hope this helps :)

--- js/admin.js
+++ js/admin.js
@@ -124,8 +124,11 @@ $.AdminBSB.leftSideBar = {
 
             //Scroll active menu item when page load, if option set = true
             if ($.AdminBSB.options.leftSideBar.scrollActiveItemWhenPageLoad) {
-                var activeItemOffsetTop = $('.menu .list li.active')[0].offsetTop
-                if (activeItemOffsetTop > 150) $el.slimscroll({ scrollTo: activeItemOffsetTop + 'px' });
+                var item = $('.menu .list li.active')[0];
+                if (item) {
+                    var activeItemOffsetTop = item.offsetTop;
+                    if (activeItemOffsetTop > 150) $el.slimscroll({ scrollTo: activeItemOffsetTop + 'px' });
+                }
             }
         }
     },
